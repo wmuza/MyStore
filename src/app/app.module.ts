38b3c593d7b1e9cdce028a0b427d58c3b5284784
Wilbert Muza <mrwilbertmuza@gmail.com>
2022-12-25 17:16:00 +0200

    Update Creating and adding necessary components into the app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,18 +9,24 @@ import { ProductsModule } from './products/products.module';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
 import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingCartComponent,
     CheckoutFormComponent,
-    OrderConfirmationComponent
+    OrderConfirmationComponent,
+    NavBarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProductsModule
+    ProductsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
