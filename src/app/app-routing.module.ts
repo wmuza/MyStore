31ffb178393096d8938bafacdcdd400ee158b3f9
Wilbert Muza <mrwilbertmuza@gmail.com>
2022-12-25 18:08:14 +0200

    Update Added product item component and Updated the routes for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
+import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { title: 'Home', path: '', component: AppComponent },
+  { title: 'Products', path: 'products', component: ProductListComponent },
+  { title: 'Product Item', path: 'product/:id', component: ProductDetailsComponent },
+  { title: 'Cart', path: 'cart', component: ShoppingCartComponent },
+  { title: 'Checkout', path: 'checkout', component: CheckoutFormComponent },
+  { title: 'Order Confirmation', path: 'order-confirmation', component: OrderConfirmationComponent },
+  { title: '404', path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    ProductItemComponent
   ],
   imports: [
     CommonModule
