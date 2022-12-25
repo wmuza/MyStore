548dc03fa6e7f3defa2524170cb66d2c78a87745
Wilbert Muza <mrwilbertmuza@gmail.com>
2022-12-25 17:42:15 +0200

    Update Added product service, product model and added them to the product list component class

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = []
+
+  constructor(private productService: ProductsService){}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data
+    })
+  }
 
 }
